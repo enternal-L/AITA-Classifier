800498e077c2c98e6844e2870f4f90a2b3563e5b
enternal-L <marvincs@umich.edu>
2024-12-24 23:28:48 -0500

    button changing

--- frontend/src/components/Content.jsx
+++ frontend/src/components/Content.jsx
@@ -21,7 +21,7 @@ const Content = ({ onSubmit, fetchChat }) => {
         <div className="flex justify-center items-center bg-white h-[60%]">
             <div className="text-center space-y-6 flex flex-col">
                 <p className="text-5xl font-semibold text-gray-700 font-sans">Are you the AH?</p>
-                <div className="flex flex-row">
+                <div className="flex flex-row relative">
                     <input
                         className="w-96 h-12 px-3 pt-2 rounded-md bg-[#d3b894] placeholder-[#3F3E3E] outline-none"
                         type="text"
@@ -30,7 +30,7 @@ const Content = ({ onSubmit, fetchChat }) => {
                         onChange={(e) => setMessage(e.target.value)}
                         onKeyDown = {handleKeyDown}
                         />
-                    <img src= {arrow} className="size-6" onClick={() => {onSubmit(message); fetchChat()}}></img>
+                    <img src= {arrow} className="absolute right-2 top-1/2 transform translate-y-[-50%] w-6 h-6 cursor-pointer" onClick={() => {onSubmit(message); fetchChat()}}></img>
                 </div>
             </div>
         </div>
