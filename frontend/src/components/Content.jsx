3609f9fd67d4139413dd12bf61c83b17303a5ed8
enternal-L <marvincs@umich.edu>
2024-12-27 21:22:59 -0500

    dynamic text box

--- frontend/src/components/Content.jsx
+++ frontend/src/components/Content.jsx
@@ -23,16 +23,22 @@ const Content = ({ onSubmit, fetchChat }) => {
         <div className="flex-center bg-white h-[93%] flex-col">
             <div className="text-center space-y-6 flex flex-col m-4">
                 <p className="text-5xl font-semibold text-gray-700 font-sans">Are you the AH?</p>
-                <div className="flex flex-row relative">
+                <div className="w-96 min-h-24 flex flex-col flex-grow rounded-md bg-[#d3b894]">
                     <textarea
-                        className="w-96 h-12 px-3 pt-2 rounded-md bg-[#d3b894] placeholder-[#3F3E3E] outline-none"
-                        type="text"
-                        placeholder="start yapping here...."
+                        className="w-full h-3/5 px-3 p-2 rounded-md bg-[#d3b894] placeholder-[#3F3E3E] outline-none resize-none overflow-hidden"
+                        placeholder="start yapping here..."
                         value={message}
-                        onChange={(e) => setMessage(e.target.value)}
-                        onKeyDown = {handleKeyDown}
-                        />
-                    <img src= {arrow} className="absolute right-2 bottom-0 transform translate-y-[-50%] w-6 h-6 cursor-pointer" onClick={() => {onSubmit(message); fetchChat(); setPercent([30, 70])}}></img>
+                        onChange={(e) => {
+                        setMessage(e.target.value);
+                        e.target.style.height = 'auto'; // Reset height to calculate scrollHeight
+                        e.target.style.height = `${e.target.scrollHeight}px`; // Adjust to text content
+                        }}
+                        onKeyDown={handleKeyDown}
+                        rows={2} // Initial row size
+                    />
+                    <div className='w-full h-11 flex-end p-2'>
+                        <img src= {arrow} className="w-6 h-6 cursor-pointer" onClick={() => {onSubmit(message); fetchChat(); setPercent([30, 70])}}></img>
+                    </div>
                 </div>
             </div>
             <History percentage={percent}/>
